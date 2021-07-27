d7b5c369c98b8da47ede34f3fc68842d534715ca
ryugaram <gkfka742@naver.com>
2021-07-27 18:08:31 +0900

    버튼 추가 및 이벤트타겟 객체 확인

--- src/components/DataGridTable.js
+++ src/components/DataGridTable.js
@@ -1,58 +1,9 @@
 import * as React from "react";
 import { DataGrid } from "@material-ui/data-grid";
 import { CircularProgress } from "@material-ui/core";
+import { Button } from "@material-ui/core";
 
 export default function DataGridTable({ kindergarden }) {
-  const columns = [
-    { field: "id", headerName: "ID", width: 60 },
-    {
-      field: "유치원",
-      headerName: "유치원",
-      width: 200,
-      editable: true,
-      sortable: false,
-      description: "정렬 불가능한 행입니다.",
-    },
-    {
-      field: "설립연도",
-      headerName: "설립연도",
-      width: 140,
-      editable: true,
-    },
-    {
-      field: "공립_사립",
-      headerName: "공립_사립",
-      width: 150,
-      editable: true,
-    },
-
-    {
-      field: "등교시간",
-      headerName: "등교시간",
-      width: 110,
-      editable: true,
-    },
-    {
-      field: "하교시간",
-      headerName: "하교시간",
-      width: 110,
-      editable: true,
-    },
-    {
-      field: "주소",
-      headerName: "주소",
-      description: "정렬 불가능한 행입니다.",
-      sortable: false,
-      width: 300,
-    },
-    {
-      field: "링크",
-      headerName: "링크",
-      width: 110,
-      editable: false,
-      sortable: false,
-    },
-  ];
   if (!kindergarden) return <CircularProgress />;
 
   let myArray = new Array();
@@ -75,13 +26,83 @@ export default function DataGridTable({ kindergarden }) {
       등교시간: goToSchool(element.opertime),
       하교시간: goToHome(element.opertime),
       주소: element.addr,
-      링크: "자세히",
+      링크: "어디로",
     });
   });
 
-  console.log(myArray[0]);
+  // console.log(myArray[0]);
 
   const rows = [...myArray];
+
+  const columns = [
+    { field: "id", headerName: "ID", width: 50 },
+    {
+      field: "유치원",
+      headerName: "유치원",
+      width: 200,
+      editable: true,
+      sortable: false,
+      description: "정렬 불가능한 행입니다.",
+    },
+    {
+      field: "설립연도",
+      headerName: "설립연도",
+      width: 140,
+      editable: true,
+    },
+    {
+      field: "공립_사립",
+      headerName: "공립_사립",
+      width: 130,
+      editable: true,
+    },
+
+    {
+      field: "등교시간",
+      headerName: "등교시간",
+      width: 125,
+      editable: true,
+    },
+    {
+      field: "하교시간",
+      headerName: "하교시간",
+      width: 125,
+      editable: true,
+    },
+    {
+      field: "주소",
+      headerName: "주소",
+      description: "정렬 불가능한 행입니다.",
+      sortable: false,
+      width: 300,
+    },
+    {
+      field: "링크",
+      headerName: "링크",
+      width: 110,
+      editable: false,
+      sortable: false,
+      renderCell: (params) => (
+        <Button
+          variant="contained"
+          color="primary"
+          size="small"
+          style={{ marginLeft: 16 }}
+          onClick={(event) => {
+            const id = params.row.id;
+            const addr = addrFilterGu(params.row.주소);
+            console.log(params);
+          }}
+        >
+          이동하기
+        </Button>
+      ),
+    },
+  ];
+
+  function addrFilterGu(addr) {
+    return addr.substring(8, 10);
+  }
 
   return (
     <div style={{ height: 500, width: "100%" }}>
@@ -95,3 +116,5 @@ export default function DataGridTable({ kindergarden }) {
     </div>
   );
 }
+
+// GridColumns 로 검색
